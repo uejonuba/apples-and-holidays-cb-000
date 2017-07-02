84815a9f1e5bb00b75ae79197e75fb83653eb39a
Uzor <uubabukoh@gmail.com>
2017-07-02 18:14:29 -0500

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,41 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+holiday_hash[:winter][:christmas] << supply
+holiday_hash[:winter][:new_years] << supply
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+holiday_hash[season] = {}
+holiday_hash[season][holiday_name] = supply_array
+holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  result = []
+  holiday_hash[:winter].each do |key, value|
+    for supply in value
+    result.push(supply)
+  end
+end
+return result
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,18 +63,42 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  result = []
+  holiday_hash.each do |season, holiday_list|
+    puts "#{text_up(season.to_s)}:"
+    holiday_list.each do |holiday_name, supply_list|
+      puts "  #{text_up(holiday_name.to_s,"_")}: #{text_up2(supply_list)}"
+    end
+  end
+#return result
 end
+
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  result = []
+  holiday_hash.each do |season, holiday|
+    holiday.each do |key, value|
+      result.push(key) if value.include?("BBQ")
+    end
+  end
+  return result
 end
 
 
+def text_up(outkey,spacer = nil)
+  outkey = outkey.split(spacer)
+  for word in outkey
+    word.capitalize!
+  end
+  return outkey.join(" ")
+end
 
 
-
-
-
+def text_up2(outkey)
+  for word in outkey
+    word = text_up(word)
+  end
+  return outkey.join(", ")
+end
